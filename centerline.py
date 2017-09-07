6763a4906ebd334e47b616ab951f0cb22b479100
tyler <tschlach@indiana.edu>
2017-09-07 13:22:20 -0400

    Class edits - Sep 7 AM
    Morning edits on class structure - windows for the suitability analysis

---
+++ centerline.py
@@ -0,0 +1,6 @@
+"""
+Defines the centerline class - which should have methods that can describe the given curve
+"""
+
+class Centerline(object):
+	def __init__(self):
--- window.py
+++ window.py
@@ -1,10 +1,27 @@
 """
 This file defines the 'window' class - which will be used to make assessments of suitability for riffles along a centercurve line
+The script can be loaded into a Python component in Grasshopper, and takes the following inputs:
+    crv: any curve
+    window_start: the point on the line to begin the window
+    window_width: the number of points in (ie length of) the window
 """
-
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
 
+###Variables that could be used by the window object class
+##
+segs = rs.CurveLength(crv) * 2
+pts = rs.DivideCurve(crv, segs)
+params = []
+for i in pts:
+    params.append(rs.CurveClosestPoint(crv,i))
+planes = []
+for i in pts:
+    planes.append(rs.PlaneFromPoints(i, ((1.0, 0.0, 0.0)),((0.0, 1.0, 0.0))))
+##
+
+
+##Creates the Window class
 class Window(object):
     def __init__(self, start, curve, pts, span):
         self.start = start
@@ -12,25 +29,39 @@ class Window(object):
         self.pts = pts
         self.span = span
     
-    def generate(self):
-        points = []
+    def generate(self): #generates a list of points that is the window
+        window = []
         for i in range(self.span):
-            points.append(self.pts[self.start+i])
-        return points
+            window.append(self.pts[self.start+i])
+        return window
 
-    def slope(self):
-    	return slope # returns the slope of the window from  the start point to the end point (start+span)
+    def slope(self): #returns the slope of the window between first and last point
+        window = self.generate()
+        s = []
+        for i in window:
+            slope = rs.Angle(window[i], window[i+1], rs.WorldXYPlane)
+            s.append()
+        return slope
+
+    def drop(self): #returns the difference in z between first and last point in the window
+        window = self.generate()
+        z = []
+        for i in window:
+            z.append(i[2])
+        drop = z[0] - z[-1]
+        return drop
+
+    def curviness(self):
+
+    def suitability(self): #the method that combines measurements in the window into a single suitability measure
+        curves = self.curviness()
+        slope = self.slope()
+        drop = self.drop()
+
+        ###the rest of the function assesses the suitability of 
 
 window = Window(window_start, crv, pts, window_width)
 
-window_out = window.generate()
+###set your output - can be generate, pts, drop etc...
 
-###Next, we need to:
-#
-##	-write a method for 'evaluating' slope
-##  -write other methods...
-##  -make more detailed comments throughout - documentation...
-##
-##
-##
-#
+window_out = window.generate() 
