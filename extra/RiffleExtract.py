3e8d025e22df5226dd37da78569bb297e3ac37aa
BioBryan <33230204+BioBryan@users.noreply.github.com>
2018-02-05 09:41:43 -0500

    Points Update

--- extra/RiffleExtract.py
+++ extra/RiffleExtract.py
@@ -3,7 +3,11 @@ import Rhino.Geometry as rg
 import math 
 
 
-points = []
+pRS = []
+pRE = []
+pPS = []
+pPE = []
+pAll = []
 paramaters = []
 lengths = []
 drops = []
@@ -11,15 +15,24 @@ tangents = []
 
 for i in crvRifflePoints.riffles:
    if i.use == 1:
-	   points.append(i.ptBankMin)
+	   pRS.append(i.ptBankMin)
+	   pRE.append(i.riffle.pt_end)
+	   pPS.append(i.pool.pt_start)
+	   pPE.append(i.pool.pt_end)
 	   paramaters.append(i.parameter)
 	   lengths.append(i.riffle.length)
 	   drops.append(i.riffle.drop)
 	   tangents.append(i.tangent)
 
+for i in crvRifflePoints.riffles:
+    pAll.append(i.ptBankMin)
 
-pts = points
 t = paramaters
 riffle_length = lengths
 riffle_drop = drops
 T = tangents
+pts_rStart = pRS
+pts_rEnd = pRE
+pts_pStart = pPS
+pts_pEnd = pPE
+allpts = pAll
